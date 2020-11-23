10dcc72d581c4e690c0a1fb0cebb026a4c0963b7
geraud18 <taffogeraud@gamail.com>
2020-11-23 13:52:28 +0100

    Landing "Ausgewählte Stellenangebote" responsive 2

--- resources/views/includes/employer_login_text.blade.php
+++ resources/views/includes/employer_login_text.blade.php
@@ -2,15 +2,15 @@
 <div class="emploginbox">
 	<div class="container">		
 		<div class="titleTop">
-			<div class="subtitle">{{__('Are You Looking For Candidates!')}}</div>
-           <h3>{{__('Post a Job Today')}}  </h3>
-			<h4>{{__('and hire the right Candidates')}}</h4>
+			<div class="subtitle">{{__('Suche nach neuen Kandidaten?')}}</div>
+           <h3>{{__('Stelle veröffentlichen')}}  </h3>
+			{{--<h4>{{__('and hire the right Candidates')}}</h4>--}}
         </div>
 		<p>
 			<!-- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nunc ex, maximus vel felis ut, vestibulum tristique enim. Proin eu nulla est. Maecenas tempor euismod suscipit. Sed at libero ante. Vestibulum nec odio lacus. -->
 
 		</p>
-		<div class="viewallbtn"><a href="{{route('register')}}">{{__('Post a Job')}}</a></div>
+		<div class="viewallbtn"><a href="{{route('register')}}">{{__('Stelle ausschreiben')}}</a></div>
 	</div>
 </div>
 @endif
--- resources/views/includes/login_text.blade.php
+++ resources/views/includes/login_text.blade.php
@@ -2,7 +2,7 @@
 <div class="userloginbox">
 	<div class="container">		
 		<div class="titleTop">
-           <h3>{{__('Are You Looking For Job!')}} </h3>
+           <h3>{{__('Auf der Suche nach Stellen?')}} </h3>
         </div>
 		<p>
 			<!-- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nunc ex, maximus vel felis ut, vestibulum tristique enim. Proin eu nulla est. Maecenas tempor euismod suscipit. Sed at libero ante. Vestibulum nec odio lacus. -->
