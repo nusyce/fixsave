310375ddbeea062d7e8749c78532e4d03845f131
nteguem roland <nteguemroland@gmail.com>
2020-11-23 11:51:39 +0100

    Landing Are You Looking For Job!

--- resources/views/includes/employer_login_text.blade.php
+++ resources/views/includes/employer_login_text.blade.php
@@ -6,7 +6,10 @@
            <h3>{{__('Post a Job Today')}}  </h3>
 			<h4>{{__('and hire the right Candidates')}}</h4>
         </div>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nunc ex, maximus vel felis ut, vestibulum tristique enim. Proin eu nulla est. Maecenas tempor euismod suscipit. Sed at libero ante. Vestibulum nec odio lacus.</p>
+		<p>
+			<!-- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nunc ex, maximus vel felis ut, vestibulum tristique enim. Proin eu nulla est. Maecenas tempor euismod suscipit. Sed at libero ante. Vestibulum nec odio lacus. -->
+
+		</p>
 		<div class="viewallbtn"><a href="{{route('register')}}">{{__('Post a Job')}}</a></div>
 	</div>
 </div>
